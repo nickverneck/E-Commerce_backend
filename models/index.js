eee0efdbbfb5055cf60553ca6e3956819a645af9
Nick Verneck <nicollas@gmail.com>
2021-05-01 20:45:26 -0400

    updated models/products.JS

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
 // Products belongsTo Category
 
 // Categories have many Products
